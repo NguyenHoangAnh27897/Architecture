244a4222b1d27c0b11d367aaff4f649e06fab9d6
NguyenHoangAnh27897 <hoanganh27897@gmail.com>
2018-05-08 12:05:13 +0700

    Khoi tao du an

---
+++ ArchitectureAndDesign/Controllers/RecruitmentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArchitectureAndDesign.Controllers
+{
+    public class RecruitmentController : Controller
+    {
+        //
+        // GET: /Recruitment/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
