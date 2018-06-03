acd153b3294e00fc565cc84439333bc5f6ea009a
NguyenHoangAnh27897 <hoanganh27897@gmail.com>
2018-06-03 12:15:07 +0700

    Update v1

---
+++ UIHumanResource/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UIHumanResource.Startup))]
+namespace UIHumanResource
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
